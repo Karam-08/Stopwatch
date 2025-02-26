3b7f15fb016f55307647dcf93d746f8e5d854528
TheProgrammerofalltime <kma69362@gmail.com>
2025-02-25 23:19:08 -0700

    commit

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,55 @@
 $(document).ready(function(){
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
+    minutes = 0;
+    seconds = 0;
+    ms = 0;
+    let running = false;
 
-    function logic(){
-
-        seconds++
-
-        if(seconds / 60 === 1){
-            seconds = 0;
-            minutes++
-
-            if(minutes / 60 === 1){
-                minutes = 0;
-                hours++
-            }
+    $("#start").on('click', function(){
+        if(running){
+            clearInterval(interval)
+            $(this).html("Start")
+            $("#lapClear").html("Clear")
+            running = false;
+        }else{
+            interval = setInterval(function(){
+                ms++
+                if(ms / 100 === 1){
+                    ms = 0;
+                    seconds++
+                }
+                if(ms.toString().length < 2){
+                    ms = "0" + ms
+                }
+                if(seconds / 60 === 1){
+                    seconds = 0;
+                    minutes++
+                }
+                if(seconds.toString().length < 2){
+                    seconds = "0" + seconds
+                }
+                if(minutes.toString().length < 2){
+                    minutes = "0" + minutes
+                }
+                
+                $("#timer").html(minutes + ":" + seconds + ":" + ms)
+                $("#lapClear").html("Lap")
+            },10)
+            $(this).html("Stop")
+            running = true;
         }
-    }
+        
+    $("#lapClear").on('click', function(){
+        if(running){
+            $("#lapdisplay").html(minutes + ":" + seconds + ":" + ms)
+        }else{
+            clearInterval(interval)
+            $("#timer").html("00:00:00")
+            minutes = 0;
+            seconds = 0;
+            ms = 0;
+        }
+    })
+
+})
+
 })
